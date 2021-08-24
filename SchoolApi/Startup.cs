e2206745faba40453d3b62f5802705e784ddba94
victor-cleber <victor.cleber@outlook.com>
2021-08-24 17:52:48 +1200

    Adding Entity Framework Core library

--- SchoolApi/Startup.cs
+++ SchoolApi/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SchoolApi.Services;
+using Microsoft.EntityFrameworkCore;
 namespace SchoolApi
 {
     public class Startup
